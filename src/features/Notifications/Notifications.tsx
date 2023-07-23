7e1ab76eb3195f4aa9107ad2ac66b36192f480d2
sjtommylee <sjtommylee@gmail.com>
2023-07-22 22:58:30 -0700

    chore: commit save point

--- src/features/Notifications/Notifications.tsx
+++ src/features/Notifications/Notifications.tsx
@@ -1,23 +1,35 @@
-import { H1, Paragraph, Separator, YStack } from 'tamagui';
+import { H1, ScrollView, Separator, Square, XStack, YStack } from 'tamagui';
+import { Box } from '../../components/atoms';
 
 export function Notifications() {
   return (
-    <YStack
-      backgroundColor={'$background'}
-      f={1}
-      jc="center"
-      ai="center"
-      p="$4"
-      space
-    >
-      <YStack space="$4" maw={600}>
+    <YStack backgroundColor={'$background'} f={1} ai="center" p="$4" space>
+      <YStack space="$4" maw={600} pt={50}>
         <H1 ta="center" fontFamily={'$silkscreen'}>
-          Notification
+          Messages
         </H1>
 
         <Separator />
       </YStack>
-      <Paragraph>Lorem ipsum</Paragraph>
+      <Box flexDirection={'column'}>
+        <ScrollView
+          width="75%"
+          backgroundColor="$background"
+          padding="$4"
+          borderRadius="$4"
+        >
+          <XStack flexWrap="wrap" alignItems="center" justifyContent="center">
+            <Square flex={1} size={120} backgroundColor="$red9" />
+            <Square size={120} backgroundColor="$yellow9" />
+            <Square size={120} backgroundColor="$blue9" />
+            <Square size={120} backgroundColor="$red9" />
+            <Square size={120} backgroundColor="$yellow9" />
+            <Square size={120} backgroundColor="$blue9" />
+            <Square size={120} backgroundColor="$pink9" />
+            <Square size={120} backgroundColor="$green10" />
+          </XStack>
+        </ScrollView>
+      </Box>
     </YStack>
   );
 }
