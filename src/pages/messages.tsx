ed28a7a918626aa7713a6d024ae8f7cf4a3d8843
sjtommylee <sjtommylee@gmail.com>
2023-07-23 02:36:51 -0700

    chore: commit save point

---
+++ src/pages/messages.tsx
@@ -0,0 +1,10 @@
+import { Messages } from '../features/Messages';
+import { Layout } from '../components';
+
+const MessagePage = () => (
+  <Layout>
+    <Messages />
+  </Layout>
+);
+
+export default MessagePage;
