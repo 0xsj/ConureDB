f4c15ce6a74401d5ed012198550ca0db719ac33b
sjtommylee <sjtommylee@gmail.com>
2023-08-13 04:03:32 -0700

    chore: commit save point

--- service/todo/src/todo/repository.rs
+++ service/todo/src/todo/repository.rs
@@ -1,4 +1,9 @@
-use crate::utils::error;
+use crate::error::make_error;
 use chrono::Utc;
 use sqlx::{FromRow, PgPool};
 use uuid::Uuid;
+
+type Error = Box<dyn std::Error + Send + Sync>;
+
+#[derive(Debug, FromRow)]
+pub struct TodoRow {}
