fb1d57f68392474c4fcb0e226483ad7ff4047eb8
sjtommylee <sjtommylee@gmail.com>
2023-09-05 14:09:47 -0700

    chore: commit save point

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -3,10 +3,6 @@ import { Flex, Screen } from "../components";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigation/navigation";
 import { HeaderBar, BottomTab } from "../components";
-import { CalendarList } from "../components/calendar/list";
-import { ContributionCalendar } from "../components/calendar/contribution";
-import AgendaScreen from "../components/calendar/agenda";
-import { AgendaList } from "../components/calendar/agenda-list";
 
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList> & {
   children?: React.ReactNode;
@@ -16,12 +12,7 @@ export function HomeScreen({ navigation }: HomeScreenProps) {
   return (
     <Screen bg="$background" edges={["top"]}>
       <HeaderBar />
-      <Flex grow justifyContent={"center"}>
-        {/* <CalendarList /> */}
-        {/* <AgendaList /> */}
-        <AgendaScreen />
-        {/* <ContributionCalendar /> */}
-      </Flex>
+      <Flex grow justifyContent={"center"}></Flex>
       <BottomTab />
     </Screen>
   );
