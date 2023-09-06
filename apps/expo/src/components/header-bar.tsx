f747e0205ed912693e13d4a40d8cc52960b4dfe3
sjtommylee <sjtommylee@gmail.com>
2023-09-06 11:53:58 -0700

    chore: commit save point

--- apps/expo/src/components/header-bar.tsx
+++ apps/expo/src/components/header-bar.tsx
@@ -3,15 +3,26 @@ import { Text, TextInput } from "./atoms";
 import { FeatherIcon } from "./atoms";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigation/navigation";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
 import { AnimatedBox } from "./layout/box";
+import { useAnimatedStyle, withTiming } from "react-native-reanimated";
 
 export const HeaderBar: React.FC = () => {
   const navigation = useNavigation();
+  const safeAreainsets = useSafeAreaInsets();
+
+  const safeAreaStyle = useAnimatedStyle(() => ({
+    // opacity: withTiming;
+  }));
 
   return (
-    <AnimatedBox position={"absolute"} top={30} right={0} left={0} px={"lg"}>
-      <AnimatedBox borderRadius={"rounded"} bg={"$header"}>
+    <AnimatedBox position={"absolute"} right={0} left={0} px={"lg"}>
+      <AnimatedBox
+        borderRadius={"rounded"}
+        bg={"$header"}
+        top={safeAreainsets.top}
+      >
         <Flex>
           <FeatherIcon name="menu" size={18} color={"grey3"} />
         </Flex>
