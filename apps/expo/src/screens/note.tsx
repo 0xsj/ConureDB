1cb7043db3f9c3dfab801b45180fe4b74b8cad28
sjtommylee <sjtommylee@gmail.com>
2023-09-06 15:27:05 -0700

    chore: commit save point

--- apps/expo/src/screens/note.tsx
+++ apps/expo/src/screens/note.tsx
@@ -1,17 +1,15 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Flex, Pressable, Screen, Text } from "../components";
 import { RootStackParamList } from "../navigation/navigation";
-import { useNavigation } from "@react-navigation/native";
 
 type NoteScreenProps = NativeStackScreenProps<RootStackParamList> & {
   children?: React.ReactNode;
 };
 
 export function NoteScreen({ navigation }: NoteScreenProps) {
-  const navigate = useNavigation();
   return (
     <Screen>
-      <Flex>
+      <Flex grow centered>
         <Pressable onPress={() => navigation.navigate("home")}>
           <Text color={"white"}>note </Text>
         </Pressable>
